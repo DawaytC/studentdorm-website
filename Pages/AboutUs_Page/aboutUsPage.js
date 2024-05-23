9e6014575bd5d26d1daed5bcac111a4ebac2aa3d
munchies09 <muniey1309@gmail.com>
2024-05-23 11:39:42 +0800

    I already edit about us page, FAQ and contact us page

---
+++ Pages/AboutUs_Page/aboutUsPage.js
@@ -0,0 +1,7 @@
+
+    document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        answer.classList.toggle('active');
+    });
+});
